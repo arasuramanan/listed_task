4d04ff3c3efc4f4ae71a1d4596b3783be7031c49
arasuramanan <arasuaudi@gmail.com>
2023-06-24 22:32:30 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Home = () => {
+  return (
+    <div className="dhiwise-navigation">
+      <h1>Homepage</h1>
+      <p className="headline">
+ 
+        <a href=""></a>
+      </p>
+      <ul>
+        <li>
+          <Link to="/signin">SignIn</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default Home;
